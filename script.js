febce6bf722c1b8b657a892196b8afb338ff690f
Ggowthampatnaik <161194915+Ggowthampatnaik@users.noreply.github.com>
2026-05-01 00:22:29 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
-console.log(30)
+document.getElementById('start-test').addEventListener('click', function() {
+    alert('Starting Communication Traits Assessment...');
+    // In a real application, this would redirect to the first question of the assessment
+});

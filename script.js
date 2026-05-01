fee410740594dd73e837c5e0ec9ba1640f975970
Ggowthampatnaik <161194915+Ggowthampatnaik@users.noreply.github.com>
2026-05-02 03:25:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,4 @@
 document.getElementById('cta').addEventListener('click', function() {
-  this.style.transform = 'scale(0.96)';
-  setTimeout(() => {
-    // Simulate navigation to the test page
-    window.location.href = '/test';
-    // Alternatively, you could use an alert:
-    // alert('Start Test clicked');
-  }, 150);
+  // Simulate navigation to the test page
+  window.location.href = '/test';
 });
